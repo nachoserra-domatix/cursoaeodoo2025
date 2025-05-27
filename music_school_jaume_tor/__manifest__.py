73e6fe19e6e4a0c27094558088dcc16cc5bfbb0e
Jaume Tor <jtor@puntoblanco.com>
2025-05-27 08:19:20 +0000

    Posar el modol a TEST de Jaume

---
+++ music_school_jaume_tor/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Music School',
+    'version': '18.0.0.0.0',
+    'description': 'Manages a music school with students, teachers, and classes.',
+    'summary': 'Manages a music school with students, teachers, and classes.',
+    'author': 'Jaume Tor',
+    'license': 'LGPL-3',
+    'category': 'Music School',
+    'depends': [
+        'base',
+    ],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/music_school_student_views.xml',
+        'views/music_school_instrument_views.xml',
+        'views/music_school_menuitems.xml',
+    ]
+}
