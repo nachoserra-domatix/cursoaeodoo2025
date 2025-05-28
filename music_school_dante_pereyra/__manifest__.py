e0f0501d77fca1933a1448266bdb92e4e5748080
DantePereyra <dante.pereyra@ciclosmontecastelo.com>
2025-05-28 16:09:39 +0000

    Task 2

--- music_school_dante_pereyra/__manifest__.py
+++ music_school_dante_pereyra/__manifest__.py
@@ -4,13 +4,16 @@
     'description': '',
     'summary': '',
     'author': 'Dante Pereyra',
-    'license': '',
+    'license': 'LGPL-3',
     'category': '',
     'depends': [
         'base',
     ],
     'data': [
         'security/ir.model.access.csv',
+        'security/res_groups.xml',
         'views/music_school_instrument_views.xml',
+        'views/music_school_student_views.xml',
+        'views/music_school_menu_views.xml',
     ],
 }
