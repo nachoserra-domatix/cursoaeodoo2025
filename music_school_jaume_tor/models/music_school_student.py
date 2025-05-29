8dc561564f262c75c9a37ebbe1f09804a7bbffa6
Jaume Tor <jtor@puntoblanco.com>
2025-05-29 08:28:55 +0000

    ultim commit

--- music_school_jaume_tor/models/music_school_student.py
+++ music_school_jaume_tor/models/music_school_student.py
@@ -1,6 +1,5 @@
 from odoo import models, fields
 
-
 class MusicSchoolStudent(models.Model):
     _name = 'music.school.student'
     _description = 'Students'
@@ -11,4 +10,21 @@ class MusicSchoolStudent(models.Model):
     phone = fields.Char(string="Phone")
     birthdate = fields.Date(string="Birthdate")
     age = fields.Integer(string="Age")
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string="Responsible",
+        help="Responsible user for this student"
+    )
+    notes = fields.Html(
+        string="Notes",
+        help="Additional notes about the student"
+    )
+
+    reference = fields.Char(
+        string="Reference",
+    )
     
+    def generate_reference(self):
+        for record in self:
+            record.reference = f"ESC-{record.id}{record.name}"
+            
